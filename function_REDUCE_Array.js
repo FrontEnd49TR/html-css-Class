0a1cb6e59b2776735a8ab2d27b3750731cfaf04c
Eugene Kuzenko <yevgenkuzenko@gmail.com>
2022-11-09 20:18:53 +0200

    function_REDUCE_Array_ForEach

--- function_REDUCE_Array.js
+++
@@ -1,6 +0,0 @@
- let ar =["123","abc","lmn","lmn"];
-function count(array, element) {
-    return array.reduce((res, cur) => cur == element ? res + 1 : res, 0);
-}
-let str ="lmn";
-console.log(`\nInput ${ar}, counter of ${str} is ${count(ar,str)}\n`)
