7c531043d394388700fd530b5badda85213cafe5
Eugene Kuzenko <yevgenkuzenko@gmail.com>
2022-11-09 19:01:49 +0200

    function_changeArray

---
+++ function_changeArray.js
@@ -0,0 +1,13 @@
+function ulSurround(strings) {
+    // let res = strings.map(function(str) {
+    //     return `<li> + ${str} + <li>`;  // return "<li>" + str + "<li>"
+    // })
+    let res = strings.map(str => `<li>${str}<li>`);  //  (str => { return <li>...})
+
+    res.unshift("<ul>"); // res.splice(0,0,"<ul>");
+    res.push("<ul>");  // res.splice(strings.length,0,"<ul>");
+    return res;
+   
+} 
+let ar =["123","abc","lmn"];
+console.log(`\n input : ${ar} output : ${ulSurround(ar)}\n`);
